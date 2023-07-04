db6136d01e75fa5d2f81a5544ced9765f2d5da30
LeonardoDiCaprio1 <111164958+LeonardoDiCaprio1@users.noreply.github.com>
2023-07-04 23:37:27 +0800

    Update get_tum.py

--- scripts/get_tum.py
+++ scripts/get_tum.py
@@ -1,32 +1,38 @@
-import os
+import numpy
 import rosbag
 import cv2
-import numpy as np
+import os
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from cv_bridge import CvBridgeError
 
-rgb_path = '/path/to/save/bag/rgb/'  # 存储提取的彩色图像的绝对路径
-depth_path = '/path/to/save/bag/depth/'  # 存储提取的深度图像的绝对路径
+rgb_path = '/home/zhao/liu/script/rgb/'
+depth_path = '/home/zhao/liu/script/depth/'
 bridge = CvBridge()
+
 num = 1
 
-with rosbag.Bag('/path/to/image.bag', 'r') as bag:
+with rosbag.Bag('/home/zhao/liu/script/image.bag', 'r') as bag:
     for topic, msg, t in bag.read_messages():
         if topic == "/camera/depth/image_raw":
-            try:
-                cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-                cv_image = cv2.normalize(cv_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-                image_name = str(num) + '.png'
-                cv2.imwrite(os.path.join(depth_path, image_name), cv_image)
-                print(os.path.join(depth_path, image_name))
-            except CvBridgeError as e:
-                print(e)
-        elif topic == "/camera/rgb/image_raw":
-            try:
-                cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-                image_name = str(num) + '.png'
-                cv2.imwrite(os.path.join(rgb_path, image_name), cv_image)
-                print(os.path.join(rgb_path, image_name))
-            except CvBridgeError as e:
-                print(e)
+            cv_image = bridge.imgmsg_to_cv2(msg, '32FC1')
+            cv_image = cv_image * 255  
+
+            image_name = str(num) + '.png'
+            cv2.imwrite(depth_path + image_name, cv_image)  
+            print(depth_path + image_name)
+
+            timestr = "%.8f" % msg.header.stamp.to_sec()
+            with open('depth.txt', 'a') as depth_time_file:
+                depth_time_file.write(timestr + " depth/" + image_name + "\n")
+
+        if topic == "/camera/rgb/image_raw": 
+            cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+            image_name = str(num) + '.png'
+            cv2.imwrite(rgb_path + image_name, cv_image)
+
+            timestr = "%.8f" % msg.header.stamp.to_sec()
+            with open('rgb.txt', 'a') as rgb_time_file:
+                rgb_time_file.write(timestr + " rgb/" + image_name + "\n")
+
         num += 1
