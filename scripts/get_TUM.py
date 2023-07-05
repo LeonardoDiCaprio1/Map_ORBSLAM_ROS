07cb7732ff9f1b6e1061826a151782252bf419c1
LeonardoDiCaprio1 <111164958+LeonardoDiCaprio1@users.noreply.github.com>
2023-07-05 13:22:40 +0800

    Update get_TUM.py

--- scripts/get_TUM.py
+++ scripts/get_TUM.py
@@ -14,28 +14,30 @@ rgb_width = None
 rgb_height = None
 
 with rosbag.Bag('/home/zhao/liu/script/image.bag', 'r') as bag:
-    with open('depth.txt', 'w') as depth_time_file, open('rgb.txt', 'w') as rgb_time_file:
+    with open('/home/zhao/liu/script/depth.txt', 'w') as depth_time_file, open('/home/zhao/liu/script/rgb.txt', 'w') as rgb_time_file:
         for topic, msg, t in bag.read_messages():
             if topic == "/camera/depth/image_raw":
-                cv_image = bridge.imgmsg_to_cv2(msg, '32FC1')
-                cv_image = cv_image * 255  
-                cv_image = cv_image.astype(np.uint16)
+                cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='32FC1')
+
+                # 将深度图像转换为mono16格式
+                cv_image_mono16 = cv2.normalize(cv_image, None, 0, 65535, cv2.NORM_MINMAX, dtype=cv2.CV_16U)
 
                 # 获取深度图像尺寸
-                depth_height, depth_width = cv_image.shape
+                depth_height, depth_width = cv_image_mono16.shape
 
                 if rgb_width is not None and rgb_height is not None:
                     # 调整深度图像尺寸为与彩色图像相同
-                    cv_image = cv2.resize(cv_image, (rgb_width, rgb_height), interpolation=cv2.INTER_LINEAR)
+                    cv_image_mono16 = cv2.resize(cv_image_mono16, (rgb_width, rgb_height), interpolation=cv2.INTER_LINEAR)
 
+                # 保存深度图像
                 image_name = str(num) + '.png'
-                cv2.imwrite(depth_path + image_name, cv_image)  
+                cv2.imwrite(depth_path + image_name, cv_image_mono16)
                 print(depth_path + image_name)
 
                 timestr = "%.8f" % msg.header.stamp.to_sec()
                 depth_time_file.write(timestr + " depth/" + image_name + "\n")
 
-            if topic == "/camera/rgb/image_raw": 
+            if topic == "/camera/rgb/image_raw":
                 cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
                 rgb_height, rgb_width, _ = cv_image.shape
                 image_name = str(num) + '.png'
